30a18005a6da2952be32826cdb26db76b9a980d4
Tiffany_Tam <tamtiffany89@gmail.com>
2021-10-14 17:59:38 -0400

    commit tree 9f188624256f84026ad5f826e68c25198400980f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(record) {
+    let winRecord = record.find (record => record.result ==="W")
+    if (winRecord) {
+        return winRecord.year
+    } else {
+    return undefined
+    }
+} 
